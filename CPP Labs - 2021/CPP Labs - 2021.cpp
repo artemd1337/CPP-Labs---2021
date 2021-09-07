6b54b70c74dc9af958ca340740ff0eef3f5ee807
artiom.danilov@outlook.com <artiom.danilov@outlook.com>
2021-09-07 16:59:55 +0400

    first test commit

---
+++ CPP Labs - 2021/CPP Labs - 2021.cpp
@@ -0,0 +1,12 @@
+﻿// CPP Labs - 2021.cpp: определяет точку входа для приложения.
+//
+
+#include "CPP Labs - 2021.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
